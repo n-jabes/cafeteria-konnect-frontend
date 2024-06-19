af075a8236d8027e0c661411325316524ea06a89
henriette BIZIYAREMYE <hopebiziyaremye@gmail.com>
2024-06-19 13:57:19 +0200

    create :LoginPage

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -16,16 +16,8 @@ const validationSchema = Yup.object().shape({
 const Login = () => {
 
   return (
-   <div>
-    <Formik>
-    initialValues ={{email:'', password:''}}
 
-
-    </Formik>
-    
-
-    
-    <div className="flex flex-col justify-center items-center h-screen bg-cover bg-no-repeat bg-center" style={{backgroundImage: `url('public/login.png')`}}>
+       <div className="flex flex-col justify-center items-center h-screen bg-cover bg-no-repeat bg-center" style={{backgroundImage: `url('public/login.png')`}}>
 
       <div className='flex flex-col w-[43%] bg-white h-[27rem] rounded-md shadow-md items-center justify-content '>
         
@@ -37,29 +29,37 @@ const Login = () => {
           <img className="w-8rem] h-[4rem]"    src="/public/BlueLogo.png" alt="" />
           </div>
         </div>
+  
+    <Formik
+    initialValues ={{email:'', password:''}}
+    validationSchema= {validationSchema}
+    onSubmit={(values)=>{
+      alert(JSON.stringify(values, null,2));
+    }}
+    >
 
-
-        <div className=' flex flex-col  items-center w-[70%] h-[17rem]  justify-center'>
-
-          <input type="email" name="" id="" placeholder='Username' className='w-full text-sm border border-[8F8F8F] px-2 py-3 m-3' />
-          <input type="password" name="" id="" placeholder='Password' className='w-full text-sm border border-[8F8F8F] px-2 py-3 m-3' />
-          <input type="button" value="Login" className='w-full px-2 text-sm py-3 bg-red-200 m-3 bg-[#078ECE] text-white font-semibold ' />
+      <Form className='flex flex-col  items-center w-[70%] h-[17rem]  justify-center'>
+      <Field name="email" type="email" placeholder='Username' className='w-full text-sm border border-[8F8F8F] px-2 py-3 m-3' />
+      <Field name="password" type="password"  placeholder='Password' className='w-full text-sm border border-[8F8F8F] px-2 py-3 m-3' />
+      <button type='submit' className='w-full px-2 text-sm py-3 m-3 bg-[#078ECE] text-white font-semibold '>Submit</button>
+      </Form>
+    </Formik>
+    <div className='w-[70%]'>
+          <p className='text-sm text-[#8F8F8F]'>Forgot your password?</p>
         </div>
-
-        <div className='w-[70%]'>
-          <p className='text-xs text-[#8F8F8F]'>Forgot your password?</p>
-        </div>
-
-      </div>
-
-      <div className='w-[20%] mt-4'>
-        <div className='w-full  '>
-        <p className='text-white text-xs capitalize'>Powered by rwanda coding academy</p>
-        </div>
-      </div>
+   
+   </div>
+    
+   <div className='w-[20%] mt-4'>
+       <div className='w-full  '>
+       <p className='text-white text-xs capitalize'>Powered by rwanda coding academy</p>
+       </div>
+     </div>
       
-    </div>
-    </div>
+  
+    
+</div>
+    
   );
 };
 
