18cb22e1c11130c1959c3c7993c9304298611a31
n-jabes <nshutij7@gmail.com>
2024-08-14 09:42:41 +0200

    update: change placeholder in login form

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -8,6 +8,7 @@ import { API_BASE_URL } from '../../utils/api';
 import { useAuth } from '../../context/AuthContext';
 
 import BlueLogo from '../../assets/BLueLogo.png';
+import Coat_of_arms from '/Coat_of_arms.png';
 import LoginBg from '../../assets/Login.png';
 
 const validationSchema = Yup.object().shape({
@@ -52,8 +53,11 @@ const Login = () => {
       className="flex flex-col justify-center items-center h-screen bg-cover bg-no-repeat bg-center"
       style={{ backgroundImage: `url(${LoginBg})` }}
     >
-      <div className="flex flex-col w-[90%] sm:w-[55%] lg:w-[43%] bg-white h-[27rem] rounded-md shadow-md items-center justify-content ">
+      <div className="flex flex-col w-[90%] sm:w-[55%] lg:w-[43%] bg-white h-[29rem] rounded-md shadow-md items-center justify-content ">
         <div className=" flex flex-col md:flex-row items-center w-[90%] md:w-[75%] mt-4 justify-center">
+          <div className="">
+            <img className="w-[4rem] h-[4rem] mr-3" src={Coat_of_arms} alt="" />
+          </div>
           <div className="mr-3">
             <h1 className="font-black text-2xl">Welcome to </h1>
           </div>
@@ -72,7 +76,7 @@ const Login = () => {
               <Field
                 name="email"
                 type="email"
-                placeholder="Username"
+                placeholder="Email"
                 className="w-full text-sm border border-[8F8F8F] px-2 py-3 m-3"
               />
               <ErrorMessage
